bdabd426ce905376357b04dcbeab60d669549e73
Koichi Sato <ksato@pepabo.com>
2017-06-26 12:44:39 +0900

    issueモデルのバリデーションを追加

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,2 +1,9 @@
 class Issue < ApplicationRecord
+  validates :title, :content, :author, presence: true
+  validates :mail_address, presence: true,  :if :notificate_to_author?
+  validates :notification_flg, inclusion:{in: [true, false]}
+
+  def notificate_to_author?
+    notification_flg == true
+  end
 end
