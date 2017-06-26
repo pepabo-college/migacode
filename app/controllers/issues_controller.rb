ac669ff638c4e76c69c884e986d6dd0403b348ff
sunecosuri <sunecosuri@pepabo.com>
2017-06-26 15:21:36 +0900

    issueのcontroller追加

---
+++ app/controllers/issues_controller.rb
@@ -0,0 +1,26 @@
+class IssuesController < ApplicationController
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+end
