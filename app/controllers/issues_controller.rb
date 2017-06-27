a66e406a40e4708d181b001679e12bf5ac5697e5
sunecosuri <sunecosuri@pepabo.com>
2017-06-27 09:46:34 +0900

    issuecontrollerの追加

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,26 +1,55 @@
 class IssuesController < ApplicationController
+  before_action :set_issue, only: [:show, :edit, :update, :destroy]
 
   def index
+    @issues = Issue.All
   end
 
   def show
   end
 
   def new
-  end
-
-  def create
-  end
-
+    @issue = Issue.new
   end
 
   def edit
   end
 
+  def create
+    @issue = Issue.new(issue_params)
+
+    respond_to do |format|
+      if @issue.save
+        format.html { redirect_to @issue, notice: '課題を作成しました' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
   def update
+    respond_to do |format|
+      if @issue.update(issue_params)
+        format.html { redirect_to @issue, notice: '課題を更新しました' }
+      else
+        format.html { render :edit }
+      end
+    end
   end
 
   def destroy
+    @issue.destroy
+    respond_to do |format|
+      format.html { redirect_to issues_url, notice: '課題を削除しました' }
+    end
   end
 
+  private
+  def set_issue
+    @issue = Issue.find(params[:id])
+  end
+
+  def issue_params
+    params.require(:issue).permit(:title, :mail_address, :content, :notification_flg, :author)
+  end
 end
