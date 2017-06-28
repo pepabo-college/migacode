cecfc877e0995541355b22b18c82ab3be83cd89c
Koichi Sato <ksato@pepabo.com>
2017-06-28 09:44:33 +0900

    メールアドレスのバリデーションをモデルに追加

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,6 +1,8 @@
 class Issue < ApplicationRecord
   validates :title, :content, :author, presence: true
-  validates :mail_address, presence: true,  if: :notificate_to_author?
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :mail_address, presence: true, format: { with: VALID_EMAIL_REGEX }, if: :notificate_to_author?
   validates :notification_flg, inclusion:{in: [true, false]}
 
   def notificate_to_author?
