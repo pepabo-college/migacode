e2a7de3a84aeb23d51fdd5766f67c01950255e22
Koichi Sato <ksato@pepabo.com>
2017-06-28 09:47:26 +0900

    変な改行が入っていたので削除

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,7 +1,6 @@
 class Issue < ApplicationRecord
   validates :title, :content, :author, presence: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-
   validates :mail_address, presence: true, format: { with: VALID_EMAIL_REGEX }, if: :notificate_to_author?
   validates :notification_flg, inclusion:{in: [true, false]}
 
