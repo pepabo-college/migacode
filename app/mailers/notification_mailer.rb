eec21132db34e8fb15bd856382ec353d70a2e2c7
Koichi Sato <ksato@pepabo.com>
2017-06-28 12:09:13 +0900

    通知メールを送信するメソッドを作成

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,2 +1,8 @@
 class NotificationMailer < ApplicationMailer
+  default from: 'notification@migacode.herokuapp.com'
+
+  def notificate_to_author(issue)
+    @issue = issue
+    mail(to: @issue.mail_address, subject: 'あなたの投稿に返信がありました。')
+  end
 end
