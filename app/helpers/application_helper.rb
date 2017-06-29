e95de71cbdeada98594b52f98913f4f3899946cb
sunecosuri <sunecosuri@pepabo.com>
2017-06-29 11:19:15 +0900

    helperメソッドにoptionの設定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+  require "redcarpet"
+
+  @@markdown = Redcarpet::Markdown.new Redcarpet::Render::HTML,
+    # <> 文字で囲まれていない場合でもリンクを解析
+    autolink: true,
+    # 単語の中で強調を解析しないようにする
+    no_intra_emphasis: true,
+    # codeをクォートでブロックとして認識する ```ruby put ARGV[0] ```
+    fenced_code_blocks: true,
+    # HTMLを出力に許可しない。
+    filter_html: true,
+    # テーブルを使用可能にする
+    tables: true,
+    # 取り消し線の解析
+    strikethrough: true
+
+  def html(markdown)
+    @@markdown.render(markdown).html_safe
+  end
 end
