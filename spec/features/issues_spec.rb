5e3128bbff257b948afc7e89c927b98bb7997fad
k.okabe <k.okabe@pmac451s.pepabo.private>
2017-06-30 11:19:26 +0900

    フィーチャースペックを追加

---
+++ spec/features/issues_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+feature 'ページ遷移' do
+
+  scenario 'トップページからコードを投稿に遷移する' do
+    visit issues_path
+    click_link '課題を追加'
+
+    expect(page).to have_css('h3', text: 'コードを投稿')
+  end
+
+  scenario 'コードを投稿からトップページに遷移する' do
+    visit new_issue_path
+    click_link '戻る'
+
+    expect(page).to have_css('h2', text: '課題一覧')
+  end
+
+end
+
+feature '登録機能' do
+  scenario 'コードを投稿する' do
+    visit new_issue_path
+    fill_in 'issue_title', with: 'テスト投稿'
+    fill_in 'issue_content', with: ' ```Ruby \ def test \ puts("test") \ end \ ```'
+    fill_in 'issue_author', with: 'test_user'
+
+    click_button 'Create Issue'
+    expect(page).to have_content '課題を作成しました'
+  end
+
+end
