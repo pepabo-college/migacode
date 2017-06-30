fa54b943f91301f3f5c7baacb1d75acdd66f7395
k.okabe <k.okabe@pmac451s.pepabo.private>
2017-06-30 11:06:12 +0900

    モデルスペックのvalidate_presence_ofで処理する内容が同じのため削除

--- spec/controllers/replies_controller_spec.rb
+++ spec/controllers/replies_controller_spec.rb
@@ -18,18 +18,12 @@ RSpec.describe RepliesController, type: :controller do
 
     context '無効な属性の場合' do
 
-      it 'データベースに新しいユーザーを保存しない' do
-        expect{
-          process :create, method: :post,
-          params: { issue_id: @issue.id, reply: attributes_for(:invalid_reply_params) }
-        }.not_to change(Reply, :count)
-      end
-
       it 'showテンプレートを再表示する' do
         process :create, method: :post,
           params: { issue_id: @issue.id, reply: attributes_for(:invalid_reply_params) }
         expect(response).to render_template "issues/show"
       end
+
     end
   end
 end
